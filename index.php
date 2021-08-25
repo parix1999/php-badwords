04878ee30e5d2fb9299ef67db0d7b6a0b9fd3140
parix1999 <parisottoedoardo@gmail.com>
2021-08-25 14:51:54 +0200

    start

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <h1>Ecco il paragrafo:</h1>
+    <?php
+        # Devo inserire un paragrafo dentro ad una variabile per poi poterlo modificare con le funzini php:
+        $testo = "<p>Il testo è un sistema in cui sono messe in relazione unità di contenuto, cui vengono attribuite forme differenti (secondo i codici e i linguaggi adoperati), e di grandezza variabile. È un testo una semplice ed essenziale frase linguistica verbale o scritta, una novella, un romanzo, un film, un quadro, un brano musicale, le istruzioni di un televisore, i gradi di una divisa militare. Sono testi tutte le unità isolabili che si possono definire come appartenenti a un sistema codificato dell'espressione</p>";
+        echo $testo;
+    ?>
+
+
+
+</body>
+</html>
