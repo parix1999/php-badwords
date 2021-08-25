e070715c8972bd2025fcdc8e1372b0f1c29fbd53
parix1999 <parisottoedoardo@gmail.com>
2021-08-25 15:16:06 +0200

    php-start

--- index.php
+++ index.php
@@ -11,8 +11,18 @@
     <h1>Ecco il paragrafo:</h1>
     <?php
         # Devo inserire un paragrafo dentro ad una variabile per poi poterlo modificare con le funzini php:
-        $testo = "<p>Il testo è un sistema in cui sono messe in relazione unità di contenuto, cui vengono attribuite forme differenti (secondo i codici e i linguaggi adoperati), e di grandezza variabile. È un testo una semplice ed essenziale frase linguistica verbale o scritta, una novella, un romanzo, un film, un quadro, un brano musicale, le istruzioni di un televisore, i gradi di una divisa militare. Sono testi tutte le unità isolabili che si possono definire come appartenenti a un sistema codificato dell'espressione</p>";
+        $testo = "<p>Il testo è un sistema in cui sono messe in relazione unità di contenuto, cui vengono attribuite forme differenti (secondo i codici e i linguaggi adoperati), e di grandezza variabile. È un testo una semplice ed essenziale frase linguistica verbale o scritta, una novella, un romanzo, un film, un quadro, un brano musicale, le istruzioni di un televisore, i gradi di una divisa militare. Sono testi tutte le unità isolabili che si possono definire come appartenenti a un sistema codificato dell'espressione.</p>";
+        # Print del testo:
         echo $testo;
+        #Lunghezza del testo:
+        $lunghezza = strlen($testo);
+        # Stampa della lunghezza testo:
+        echo "Lunghezza del testo complessivo: {$lunghezza} <br>";
+        
+        # Passo la parola da censurare tramite parametro GET :
+        $badWord = $_GET["parola"];
+        #Devo sostituire la parola inserita con degli asterischi: 
+
     ?>
 
 
