fae63954777dcb5a85b9a9e4e74300ce7f60ec4c
parix1999 <parisottoedoardo@gmail.com>
2021-08-25 16:28:51 +0200

    php

---
+++ bonus.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <h1>Ecco il paragrafo:</h1>
+    <?php
+        # Devo inserire un paragrafo dentro ad una variabile per poi poterlo modificare con le funzini php:
+        $testo = "<p>Il testo è un sistema in cui sono messe in relazione unità di contenuto, cui vengono attribuite forme differenti (secondo i codici e i linguaggi adoperati), e di grandezza variabile. È un testo una semplice ed essenziale frase linguistica verbale o scritta, una novella, un romanzo, un film, un quadro, un brano musicale, le istruzioni di un televisore, i gradi di una divisa militare. Sono testi tutte le unità isolabili che si possono definire come appartenenti a un sistema codificato dell'espressione.</p>";
+        # Print del testo:
+        echo $testo;
+        #Lunghezza del testo:
+        $lunghezza = strlen($testo);
+        # Stampa della lunghezza testo:
+        echo "Lunghezza del testo complessivo: {$lunghezza} <br>";
+        
+        # Passo la parola da censurare tramite parametro GET :
+        $badWord = $_GET["parola"];
+        # Devo sostituire la parola inserita con degli asterischi solo se la parola è più lunga di 1:
+        # Cosi prima si visualizza il testo e poi si inserisce la parola e verrà stampata in seguito il testo con
+        # La censura
+        if (strlen($badWord) > 1) {
+            $censura = str_replace($badWord, '***', $testo );
+        }
+        
+        echo $censura;
+
+
+
+    ?>
+
+
+
+</body>
+</html>
